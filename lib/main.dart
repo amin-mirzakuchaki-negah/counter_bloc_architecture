c7f0518a8e7cd9f7afb5d616a5cd24ef46c70e00
amin-mirzakuchaki-negah <amin.mirzakuchaki.negah@gmail.com>
2023-08-01 12:03:23 +0330

    Added CounterApp as home in app.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:counter_bloc_arch/app.dart';
 import 'package:counter_bloc_arch/counter_observer.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'counter/view/counter_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  Bloc.observer = const CounterObserver();
+  runApp(const CounterApp());
 }
